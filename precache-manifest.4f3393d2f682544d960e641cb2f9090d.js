0e4b88e1cbea24d24bbf291586a539c24dc8edff
VictorBramy <53178184+VictorBramy@users.noreply.github.com>
2020-03-28 16:26:58 +0300

    Updates

---
+++ precache-manifest.4f3393d2f682544d960e641cb2f9090d.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fb304de4d9c5a4e7b1a96bb3e2954a0",
+    "url": "/tiferet-ashemesh/index.html"
+  },
+  {
+    "revision": "204cd2d016c1921f9acb",
+    "url": "/tiferet-ashemesh/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "e68790cc378a245617eb",
+    "url": "/tiferet-ashemesh/static/css/main.66983dfd.chunk.css"
+  },
+  {
+    "revision": "204cd2d016c1921f9acb",
+    "url": "/tiferet-ashemesh/static/js/2.e7dfe135.chunk.js"
+  },
+  {
+    "revision": "e68790cc378a245617eb",
+    "url": "/tiferet-ashemesh/static/js/main.4c0b5ecb.chunk.js"
+  },
+  {
+    "revision": "49c3bf4efaa3ef7d158c",
+    "url": "/tiferet-ashemesh/static/js/runtime~main.689798cf.js"
+  },
+  {
+    "revision": "03557f483a5a065c20bc034c3246c9bd",
+    "url": "/tiferet-ashemesh/static/media/1.03557f48.mp4"
+  },
+  {
+    "revision": "99938effbd833f22eb3c7bb2db4e5c61",
+    "url": "/tiferet-ashemesh/static/media/1.99938eff.jpg"
+  },
+  {
+    "revision": "2fe466a8d1364f2e1baf86f2fdc951dd",
+    "url": "/tiferet-ashemesh/static/media/10.2fe466a8.jpg"
+  },
+  {
+    "revision": "4124562b6fd72253121e8c91db0f4548",
+    "url": "/tiferet-ashemesh/static/media/10.4124562b.mp4"
+  },
+  {
+    "revision": "8334bc594b552d9712d6f704e1280384",
+    "url": "/tiferet-ashemesh/static/media/11.8334bc59.mp4"
+  },
+  {
+    "revision": "ebfa54bd7b2eca60841418554340006e",
+    "url": "/tiferet-ashemesh/static/media/11.ebfa54bd.jpg"
+  },
+  {
+    "revision": "d9886a0bc88681956c762a2e5a95d83d",
+    "url": "/tiferet-ashemesh/static/media/12.d9886a0b.jpg"
+  },
+  {
+    "revision": "a139b4bbe2f77db283f6c437d5d61792",
+    "url": "/tiferet-ashemesh/static/media/13.a139b4bb.jpg"
+  },
+  {
+    "revision": "caea2a50e47f669d588818747b14b9b6",
+    "url": "/tiferet-ashemesh/static/media/14.caea2a50.jpg"
+  },
+  {
+    "revision": "c271ff738c426f587d9a6b9e49d3b835",
+    "url": "/tiferet-ashemesh/static/media/15.c271ff73.jpg"
+  },
+  {
+    "revision": "56c9532194522a779070b65628127f45",
+    "url": "/tiferet-ashemesh/static/media/16.56c95321.jpg"
+  },
+  {
+    "revision": "a3ad3ece14605a2b7dea84be534faa6e",
+    "url": "/tiferet-ashemesh/static/media/17.a3ad3ece.jpg"
+  },
+  {
+    "revision": "b2e635fe43941c30d0fe94433a66c4b3",
+    "url": "/tiferet-ashemesh/static/media/2.b2e635fe.jpg"
+  },
+  {
+    "revision": "f5a6140e30073f246c1dd0b2b6b2b1f9",
+    "url": "/tiferet-ashemesh/static/media/2.f5a6140e.mp4"
+  },
+  {
+    "revision": "4f6a688b2b84ace74c5c4d680f2edb9f",
+    "url": "/tiferet-ashemesh/static/media/3.4f6a688b.jpg"
+  },
+  {
+    "revision": "63fd666fa20fcfeee07bde9c93df50d0",
+    "url": "/tiferet-ashemesh/static/media/3.63fd666f.mp4"
+  },
+  {
+    "revision": "0c90935b801a7908971459cc16174ac0",
+    "url": "/tiferet-ashemesh/static/media/4.0c90935b.jpg"
+  },
+  {
+    "revision": "f9d52be8e7b632317935bfe141b49f8d",
+    "url": "/tiferet-ashemesh/static/media/4.f9d52be8.mp4"
+  },
+  {
+    "revision": "b54a88f5283930341879cb73ab78e4f1",
+    "url": "/tiferet-ashemesh/static/media/5.b54a88f5.jpg"
+  },
+  {
+    "revision": "bce1a0952d0c55d7dc630f7115506cc8",
+    "url": "/tiferet-ashemesh/static/media/5.bce1a095.mp4"
+  },
+  {
+    "revision": "7043a0190694f8db29fc6c17e1e6b458",
+    "url": "/tiferet-ashemesh/static/media/6.7043a019.jpg"
+  },
+  {
+    "revision": "b73c786baf1def3d72030807fc621c47",
+    "url": "/tiferet-ashemesh/static/media/6.b73c786b.mp4"
+  },
+  {
+    "revision": "1bdadff12364661fed0d79fd0154c5ca",
+    "url": "/tiferet-ashemesh/static/media/7.1bdadff1.jpg"
+  },
+  {
+    "revision": "9ae93c619471c6c39b66c89355af7c39",
+    "url": "/tiferet-ashemesh/static/media/7.9ae93c61.mp4"
+  },
+  {
+    "revision": "6f05c6c368e3a3dd417a84133e39157b",
+    "url": "/tiferet-ashemesh/static/media/8.6f05c6c3.jpg"
+  },
+  {
+    "revision": "bbfae7bfd2b1d11a087378a0dd5a6a6f",
+    "url": "/tiferet-ashemesh/static/media/8.bbfae7bf.mp4"
+  },
+  {
+    "revision": "179b981937fe56037fb76b4633c54996",
+    "url": "/tiferet-ashemesh/static/media/9.179b9819.mp4"
+  },
+  {
+    "revision": "18f37918c5adbfc133bc0fd7b6af6ba1",
+    "url": "/tiferet-ashemesh/static/media/9.18f37918.jpg"
+  },
+  {
+    "revision": "f807b37b1736e7709918ea35ef80ca7d",
+    "url": "/tiferet-ashemesh/static/media/bg3.f807b37b.jpg"
+  },
+  {
+    "revision": "d7d1aaf71d275992031bd75cdb6ec65c",
+    "url": "/tiferet-ashemesh/static/media/bg6.d7d1aaf7.jpg"
+  },
+  {
+    "revision": "607a4a36d058e55773625ba23518e291",
+    "url": "/tiferet-ashemesh/static/media/bg7.607a4a36.jpg"
+  },
+  {
+    "revision": "6737ce3b2577a1c3977e4d479511f642",
+    "url": "/tiferet-ashemesh/static/media/bg8.6737ce3b.jpg"
+  },
+  {
+    "revision": "bfd5efa5140b51e3d2ec98cf31b6fd1e",
+    "url": "/tiferet-ashemesh/static/media/home-52.bfd5efa5.svg"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/tiferet-ashemesh/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/tiferet-ashemesh/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/tiferet-ashemesh/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/tiferet-ashemesh/static/media/nucleo-outline.8ebec31f.woff2"
+  },
+  {
+    "revision": "d63786960f2233db61d4662d3574238d",
+    "url": "/tiferet-ashemesh/static/media/phone-2.d6378696.svg"
+  }
+]);
